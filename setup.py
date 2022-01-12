dd4293596679eb40a2091b7c17e0dfdaa82189a8
simoninithomas <simonini_thomas@outlook.fr>
2022-01-12 09:18:41 +0100

    Updated install requirements in setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,6 @@
 from setuptools import setup
 
 install_requires = [
-    "logging",
-    "os",
-    "urllib",
-    "shutil",
-    "pathlib",
     "huggingface_hub",
 ]
 
@@ -18,6 +13,6 @@ setup(
     author='Thomas Simonini',
     author_email='thomas.simonini@huggingface.co',
     description='Additional code for Stable-baselines3 to load and upload models from the Hub.',
-    install_requires=install_requires
+    install_requires=install_requires,
 
 )
