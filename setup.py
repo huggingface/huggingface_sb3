294c52bc0fbf6562e67d654845c27efeafe30d91
Thomas Simonini <simonini.thomas.pro@gmail.com>
2022-01-18 06:48:27 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,13 +6,17 @@ install_requires = [
 
 setup(
     name='huggingface_sb3',
-    version='1.0',
+    version='1.0.2',
     packages=['huggingface_sb3'],
     url='https://github.com/huggingface/huggingface_sb3',
-    license='',
-    author='Thomas Simonini',
+    license='Apache',
+    author='Thomas Simonini, Omar Sanseviero and Hugging Face Team',
     author_email='thomas.simonini@huggingface.co',
     description='Additional code for Stable-baselines3 to load and upload models from the Hub.',
     install_requires=install_requires,
-
+    long_description=open("README.md", "r", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    keywords="reinforcement learning deep reinforcement learning RL",
 )
+
+
