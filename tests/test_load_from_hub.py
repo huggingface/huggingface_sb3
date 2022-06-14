2289acd4aa76a0a7f6279fb5c6d87ea5a5c7be48
Maximilian Ernestus <maximilian@ernestus.de>
2022-06-14 10:36:44 +0200

    Add smoke test for loading a model from the huggingface hub.

---
+++ tests/test_load_from_hub.py
@@ -0,0 +1,25 @@
+import gym
+
+from huggingface_sb3 import load_from_hub, RepoId, ModelName, EnvironmentName
+from stable_baselines3 import PPO
+from stable_baselines3.common.evaluation import evaluate_policy
+
+
+def test_load_from_hub():
+    # Retrieve the model from the hub
+    ## repo_id = id of the model repository from the Hugging Face Hub (repo_id = {organization}/{repo_name})
+    ## filename = name of the model zip file from the repository
+    environment_name = EnvironmentName("CartPole-v1")
+    model_name = ModelName("ppo", environment_name)
+    checkpoint = load_from_hub(
+        repo_id=RepoId("sb3", model_name),
+        filename=model_name.filename,
+    )
+    model = PPO.load(checkpoint)
+
+    # Evaluate the agent and watch it
+    eval_env = gym.make(environment_name.gym_id)
+    mean_reward, std_reward = evaluate_policy(
+        model, eval_env, render=False, n_eval_episodes=5, deterministic=True, warn=False
+    )
+    print(f"mean_reward={mean_reward:.2f} +/- {std_reward}")
