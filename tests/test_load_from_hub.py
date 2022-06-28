0f89405439339f77bf33d48f865d4173c196abf1
Maximilian Ernestus <maximilian@ernestus.de>
2022-06-28 13:24:00 +0200

    Add test for loading from hub without naming scheme.

--- tests/test_load_from_hub.py
+++ tests/test_load_from_hub.py
@@ -23,3 +23,21 @@ def test_load_from_hub_with_naming_scheme_utils():
         model, eval_env, render=False, n_eval_episodes=5, deterministic=True, warn=False
     )
     print(f"mean_reward={mean_reward:.2f} +/- {std_reward}")
+
+
+def test_load_from_hub():
+    # Retrieve the model from the hub
+    ## repo_id = id of the model repository from the Hugging Face Hub (repo_id = {organization}/{repo_name})
+    ## filename = name of the model zip file from the repository
+    checkpoint = load_from_hub(
+        repo_id="sb3/ppo-CartPole-v1",
+        filename="ppo-CartPole-v1.zip",
+    )
+    model = PPO.load(checkpoint)
+
+    # Evaluate the agent and watch it
+    eval_env = gym.make("CartPole-v1")
+    mean_reward, std_reward = evaluate_policy(
+        model, eval_env, render=False, n_eval_episodes=5, deterministic=True, warn=False
+    )
+    print(f"mean_reward={mean_reward:.2f} +/- {std_reward}")
