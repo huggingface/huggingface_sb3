6e834fa5f699c160abde48ca9ce58c13966ed36b
Maximilian Ernestus <maximilian@ernestus.de>
2023-07-05 12:55:48 +0200

    Add missing dependencies.

--- setup.py
+++ setup.py
@@ -5,7 +5,9 @@ install_requires = [
     "pyyaml~=6.0",
     "wasabi",
     "numpy",
-    "cloudpickle>=1.6"
+    "cloudpickle>=1.6",
+    "gym~=0.21",
+    "stable-baselines3~=1.7",
 ]
 
 extras = {}
