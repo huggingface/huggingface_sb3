ced487d4701f14bcd4d22ee9244805224c8b8e34
Maximilian Ernestus <maximilian@ernestus.de>
2023-07-05 12:55:39 +0200

    Add tests for new feature.

--- tests/test_naming_scheme.py
+++ tests/test_naming_scheme.py
@@ -3,7 +3,7 @@ import pytest
 from huggingface_sb3 import EnvironmentName, ModelName, ModelRepoId
 
 
-@pytest.fixture(params=["seals/Walker2d-v0", "LunarLander-v2"])
+@pytest.fixture(params=["seals/Walker2d-v0", "LunarLander-v2", "seals:seals/Walker2d-v0"])
 def env_id(request) -> str:
     return request.param
 
@@ -23,11 +23,23 @@ def repo_id(model_name: ModelName) -> ModelRepoId:
     return ModelRepoId("orga", model_name)
 
 
-def test_that_slashes_are_removed(env_id: str, env_name: EnvironmentName, model_name: ModelName, repo_id: ModelRepoId):
+def test_that_slashes_are_removed(env_name: EnvironmentName, model_name: ModelName, repo_id: ModelRepoId):
     assert "/" not in env_name
     assert "/" not in model_name
     assert "/" not in model_name.filename
     assert repo_id.count("/") == 1  # note: repo id has exactly one slash separating org from repo name
+
+
+def test_that_colon_is_removed(env_name: EnvironmentName, model_name: ModelName, repo_id: ModelRepoId):
+    assert ":" not in env_name
+    assert ":" not in model_name
+    assert ":" not in model_name.filename
+    assert ":" not in repo_id
+
+
+def test_that_package_before_colon_is_removed():
+    env_name = EnvironmentName("seals:seals/Walker2d-v0")
+    assert env_name == "seals-Walker2d-v0"
 
 
 def test_that_gym_id_is_preserved(env_id: str, env_name: EnvironmentName):
