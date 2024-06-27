8cbd895122319b2f9aa09e73be39f3a3236e4cb4
Quentin Gallouédec <45557362+qgallouedec@users.noreply.github.com>
2024-06-27 18:16:11 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ extras["quality"] = [
     "flake8>=3.8.3",
 ]
 
-extras["test"] = ["pytest"]
+extras["test"] = ["pytest", "gymnasium[classic-control"]
 
 extras["dev"] = extras["quality"] + extras["test"]
 
