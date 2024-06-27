35ecbb9af7312c38e308823419271ddd98e08e84
Lucain <lucainp@gmail.com>
2024-06-27 18:08:12 +0200

    Review `huggingface_hub` integration (#43)
    * Update push_to_hub.py
    
    * Update setup.py
    
    * Update README.md
    
    * test push to hub
    
    * format
    
    * build
    
    * drop python 3.7
    
    * Update setup.py
    
    * Update build.yml
    
    * Update setup.py
    
    * Update setup.py
    
    * Update build.yml
    
    * install shimmy for test
    
    * Pin setup tools version in test
    
    * Update setup.py
    
    * Update build.yml
    
    * Update build.yml
    
    * Update build.yml
    
    * Update build.yml
    
    * Update test_load_from_hub.py
    
    * import sys
    
    * gym==0.21
    
    * setuptools
    
    * setuptools
    
    * pip wheel together
    
    * don't upgrade pip
    
    * drop gym test
    
    * Create blank
    
    * Rename blank to blank.yml
    
    * Update blank.yml
    
    * Delete .github/workflows/blank.yml
    
    * Update build.yml
    
    * Update build.yml
    
    ---------
    
    Co-authored-by: Quentin Gallouédec <quentin.gallouedec@huggingface.co>
    Co-authored-by: Quentin Gallouédec <45557362+qgallouedec@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup
 
 install_requires = [
-    "huggingface_hub~=0.8",
+    "huggingface_hub>=0.21",
     "pyyaml~=6.0",
     "wasabi",
     "numpy",
     "cloudpickle>=1.6",
+    "stable-baselines3",
 ]
 
 extras = {}
@@ -16,15 +17,19 @@ extras["quality"] = [
     "flake8>=3.8.3",
 ]
 
+extras["test"] = ["pytest"]
+
+extras["dev"] = extras["quality"] + extras["test"]
+
 setup(
-    name='huggingface_sb3',
-    version='3.0',
-    packages=['huggingface_sb3'],
-    url='https://github.com/huggingface/huggingface_sb3',
-    license='Apache',
-    author='Thomas Simonini, Omar Sanseviero and Hugging Face Team',
-    author_email='thomas.simonini@huggingface.co',
-    description='Additional code for Stable-baselines3 to load and upload models from the Hub.',
+    name="huggingface_sb3",
+    version="3.1",
+    packages=["huggingface_sb3"],
+    url="https://github.com/huggingface/huggingface_sb3",
+    license="Apache",
+    author="Thomas Simonini, Omar Sanseviero and Hugging Face Team",
+    author_email="thomas.simonini@huggingface.co",
+    description="Additional code for Stable-baselines3 to load and upload models from the Hub.",
     install_requires=install_requires,
     extras_require=extras,
     long_description=open("README.md", "r", encoding="utf-8").read(),
